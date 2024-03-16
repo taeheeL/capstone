233535c770dcbc76cb3cd19ab96da3b3b12ac63f
HaeTi <t9h9@naver.com>
2024-03-16 21:28:13 +0900

    feature home screen UI

--- app/src/main/java/com/haeti/capstone/home/HomeScreen.kt
+++ app/src/main/java/com/haeti/capstone/home/HomeScreen.kt
@@ -1,12 +1,96 @@
 package com.haeti.capstone.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.haeti.capstone.R
 import com.haeti.capstone.main.MainViewModel
+import com.haeti.capstone.navigation.Screen
+import com.haeti.capstone.ui.theme.BlackGray
 
 @Composable
 fun HomeScreen(
     mainViewModel: MainViewModel,
     navController: NavController
 ) {
+    Scaffold {
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize()
+                .background(Color.White),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+
+        ) {
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Button(
+                onClick = { navController.navigate(Screen.Main.route) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp, vertical = 60.dp)
+                    .height(60.dp),
+                shape = RoundedCornerShape(12.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = BlackGray,
+                    contentColor = Color.White,
+                )
+            ) {
+                Text(
+                    text = "Get Started", style = MaterialTheme.typography.titleMedium.copy(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp
+                    )
+                )
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                modifier = Modifier
+                    .size(300.dp)
+                    .padding(bottom = 80.dp),
+                painter = painterResource(id = R.drawable.icon_with_text),
+                contentDescription = null,
+            )
+        }
+    }
+
+}
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun HomeScreenPreview() {
+    HomeScreen(mainViewModel = MainViewModel(), navController = rememberNavController())
 }
