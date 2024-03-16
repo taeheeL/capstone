f51eb3f14e37cf40ace3a3be003bebdd8d315573
HaeTi <t9h9@naver.com>
2024-03-17 03:13:36 +0900

    feature ResultScreen UI

--- app/src/main/java/com/haeti/capstone/result/ResultScreen.kt
+++ app/src/main/java/com/haeti/capstone/result/ResultScreen.kt
@@ -1,12 +1,100 @@
 package com.haeti.capstone.result
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.haeti.capstone.R
 import com.haeti.capstone.main.MainViewModel
+import com.haeti.capstone.navigation.Screen
+import com.haeti.capstone.ui.theme.BlackGray
 
 @Composable
 fun ResultScreen(
     mainViewModel: MainViewModel,
     navController: NavController
 ) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Spacer(modifier = Modifier.weight(1f))
+
+        Button(
+            onClick = { navController.navigate(Screen.Main.route) },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp, vertical = 40.dp)
+                .height(60.dp),
+            shape = RoundedCornerShape(12.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = BlackGray,
+                contentColor = Color.White,
+            )
+        ) {
+            Text(
+                text = "Go Home", style = MaterialTheme.typography.titleMedium.copy(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp
+                )
+            )
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier
+                    .size(300.dp)
+                    .padding(bottom = 80.dp),
+                painter = painterResource(id = R.drawable.img_healthy),
+                contentDescription = null,
+            )
+
+            Text(
+                text = "You are healthy!",
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun ResultScreenPreview() {
+    ResultScreen(mainViewModel = MainViewModel(), navController = rememberNavController())
 }
