9d7e3919b524de17b07932a476b97d3c3b7eb24d
HaeTi <t9h9@naver.com>
2024-03-16 23:55:37 +0900

    feature TitleText

---
+++ app/src/main/java/com/haeti/capstone/main/components/TitleText.kt
@@ -0,0 +1,30 @@
+package com.haeti.capstone.main.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TitleText(
+    modifier: Modifier = Modifier,
+    text: String,
+) {
+    Text(
+        text = text,
+        modifier = modifier,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.SemiBold,
+        color = Color.Black
+    )
+}
+
+@Composable
+@Preview(showSystemUi = true, showBackground = true)
+fun TitleTextPreview(
+) {
+    TitleText(text = "Height")
+}
